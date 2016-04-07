e72b23b311be9f0d00a0960e3f57075ea342d2e4
topangad <olga.bool@yahoo.ca>
2016-04-06 23:21:32 -0700

    finished implementing gradient descent logic for linlinear reg/line of best fit

--- MachineLearning.java
+++ MachineLearning.java
@@ -48,29 +48,72 @@ class GradientDescent  {
       this.points = points;
    }
    
-   public double CostFunctionLeastSquares(List<Point> points, linearHypothesis mxb) {
+   //y = mx + b abbr = mxb, returns the derivative of the cost function
+   //evaluated at J( theta1, theta2)s
+   public double CostFunctionLeastSquares(
+      List<Point> points,
+      linearHypothesis mxb,
+      int derivativeParams
+   ){   
       double sum = 0;
       int numTrainingExamples = points.size();
 
       for ( int i = 0 ; i < numTrainingExamples; i ++ ) {
-         double yprime = mxb.evalAt(points.get(i).x) - points.get(i).y;
-         sum += yprime*yprime;
+         double yprime, x, y;
+         x = points.get(i).x;
+         y = points.get(i).y;
+         
+         switch( derivativeParams ) {
+            case 1:
+               yprime = (mxb.evalAt(x) - y) ; 
+               sum += yprime;
+               break;
+            case 2:
+               yprime = (mxb.evalAt(x) - y)*x;
+               sum+= yprime;
+               break;
+            default:
+               yprime = mxb.evalAt(x) - y;
+               sum += yprime*yprime;
+         }
       }
 
       sum = sum/(2*numTrainingExamples);
-      return sum;
+      
+      // if case 1 or 2, fomula for derivaive calculations is 1/m instead of 1/2m     
+      return ( derivativeParams != 0 ) ? sum*2 : sum ;
    }
    
-   public linearHypothesis getLineOfBestFit() {
-      for ( int i = 0 ; i < this.points.size() ; i ++ ) {
+   public linearHypothesis getLineOfBestFit(double theta1, double theta2) {
+   
+      double alpha = 0.3; //rate of convergence
+      double theta1new = 4000, theta2new = 4000;
+      boolean skipFirst = true;
+      
+      while (!skipFirst && theta1new - theta1 < 0.0005){
+         if(skipFirst) {
+            skipFirst = false;
+         }
+      
+         linearHypothesis mxb = new linearHypothesis( theta1, theta2);
          
+         double derivativeCostFunctionO1 = CostFunctionLeastSquares(this.points,mxb, 1);
+         double derivativeCostFunctionO2 = CostFunctionLeastSquares(this.points,mxb, 2);                  
+
+         theta1new = theta1 - alpha*derivativeCostFunctionO1;
+         theta2new = theta2 - alpha*derivativeCostFunctionO2;
+         
+         //update linear hypothesis parameters and cost function minimization
+         theta1 = theta1new;
+         theta2 = theta2new;
       }
-      return new linearHypothesis( 8 , 8 );
+      
+      return new linearHypothesis(theta1new, theta2new) ;
    }
    
    public static void main( String [] args) {
       Plot plot = Plot.plot(Plot.plotOpts().
-				title("Hello World").
+				title("Gradient Descent Algorithm with 1 Var").
 				legend(Plot.LegendFormat.BOTTOM)).
 			xAxis("x", Plot.axisOpts().
 				range(0, 5)).
